1ed111a2d07189cdbd009902eef829320b922137
Shanks <shanks@havenlife.com>
2018-06-21 14:17:46 -0400

    none of the files in the previous commit were actually committed

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-import { ObjectTemplate } from './ObjectTemplate';
-export default ObjectTemplate;
-declare let amorphicStatic: typeof ObjectTemplate;
-declare let SupertypeSession: typeof ObjectTemplate;
-export { amorphicStatic, SupertypeSession };
-export { SupertypeLogger } from './SupertypeLogger';
-export { Supertype } from './Supertype';
-export { supertypeClass, property, remote } from './decorators';
